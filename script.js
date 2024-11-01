a6fb67483b09da370a1dd167eb5e898c5b17bb7c
OkemwaEnid <okemwaenid23@gmail.com>
2024-11-01 21:58:32 +0300

    edit contact page

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    const splashScreen = document.getElementById("splash-screen");
-    const mainContent = document.getElementById("main-content");
+// document.addEventListener("DOMContentLoaded", function () {
+//   setTimeout(function () {
+//     const splashScreen = document.getElementById("splash-screen");
+//     const mainContent = document.getElementById("main-content");
 
-    // Add the 'hidden' class to fade out the splash screen
-    // splashScreen.classList.add("hidden");
+//     // Add the 'hidden' class to fade out the splash screen
+//     // splashScreen.classList.add("hidden");
 
-    setTimeout(function () {
-      splashScreen.style.display = "none";
-      mainContent.style.display = "block";
-    }, 500);
-  }, 2000);
-});
+//     setTimeout(function () {
+//       splashScreen.style.display = "none";
+//       mainContent.style.display = "block";
+//     }, 500);
+//   }, 2000);
+// });
